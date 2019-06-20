5d9da354249965993aa08ef4739d3572b6125c47
himanshi2626 <51352683+himanshi2626@users.noreply.github.com>
2019-06-20 11:28:52 +0530

    Update testNGBatchFile.bat

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\slide53\eclipse-workspace\SlideTeamFormsAuto
+set projectLocation=https://github.com/himanshi2626/SlideTeamFormsAuto
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
